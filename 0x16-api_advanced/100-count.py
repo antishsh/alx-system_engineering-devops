35e04b7df68782277ae34c703b0891fe4338a9d1
antish <AntenehShi@ethiopianairlines.com>
2022-12-02 06:03:00 +0300

    corr

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""Contains the count_words function"""
+"""Contains the count_words function."""
 import requests
 
 
 def count_words(subreddit, word_list, found_list=[], after=None):
-    '''Prints counts of given words found in hot posts of a given subreddit.
+    """Print counts of given words found in hot posts of a given subreddit.
+
     Args:
         subreddit (str): The subreddit to search.
         word_list (list): The list of words to search for in post titles.
         found_list (obj): Key/value pairs of words/counts.
         after (str): The parameter for the next page of the API results.
-    '''
+    """
     user_agent = {'User-agent': 'test45'}
     posts = requests.get('http://www.reddit.com/r/{}/hot.json?after={}'
                          .format(subreddit, after), headers=user_agent)
